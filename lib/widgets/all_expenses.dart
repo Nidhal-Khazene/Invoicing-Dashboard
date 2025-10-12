befb82f85c46333050216dcbbfa20bfc4b730929
nidhal khazene <nidhal.khazene@gmail.com>
2025-10-12 09:36:23 +0100

    refector and add custom background container

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -2,27 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:invoicing_dashboard/widgets/all_expenses_items_list_view.dart';
 
 import 'all_expenses_header.dart';
+import 'custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      ),
-      child: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            AllExpensesHeader(),
-            SizedBox(height: 16),
-            AllExpensesItemsListView(),
-          ],
-        ),
+    return const CustomBackgroundContainer(
+      child: Column(
+        children: [
+          AllExpensesHeader(),
+          SizedBox(height: 16),
+          AllExpensesItemsListView(),
+        ],
       ),
     );
   }
