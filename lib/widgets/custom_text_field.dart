410e179a20177c09a4c990215e1052c3bc63c450
nidhal khazene <nidhal.khazene@gmail.com>
2025-10-15 09:29:16 +0100

    add title text field

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:invoicing_dashboard/utils/app_styles.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hintText});
+  final String hintText;
   @override
   Widget build(BuildContext context) {
     return TextField(
       decoration: InputDecoration(
         fillColor: const Color(0xffFAFAFA),
         filled: true,
+        border: buildOutlineInputBorder(),
         enabledBorder: buildOutlineInputBorder(),
         focusedBorder: buildOutlineInputBorder(),
-        hintText: "Type customer name",
+        hintText: hintText,
         hintStyle: AppStyles.regular16,
       ),
     );
