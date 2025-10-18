2bf587c2822c7e57f250ad7a1e7039f3d37ca4d9
nidhal khazene <nidhal.khazene@gmail.com>
2025-10-18 12:19:10 +0100

    add transection list tile model

---
+++ lib/widgets/transection_list_tile_model.dart
@@ -0,0 +1,9 @@
+class TransectionListTileModel {
+  final String title, subtitle, trailingTitle;
+
+  const TransectionListTileModel({
+    required this.title,
+    required this.subtitle,
+    required this.trailingTitle,
+  });
+}
