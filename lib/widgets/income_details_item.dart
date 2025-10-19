5b832515a466a4906be82aed7422f4fd7fa0a1c4
nidhal khazene <nidhal.khazene@gmail.com>
2025-10-19 13:52:20 +0100

    refactor icome details item

--- lib/widgets/income_details_item.dart
+++ lib/widgets/income_details_item.dart
@@ -5,30 +5,28 @@ import 'package:invoicing_dashboard/widgets/income_details_item_model.dart';
 
 class IncomeDetailsItem extends StatelessWidget {
   const IncomeDetailsItem({super.key, required this.itemModel});
+
   final IncomeDetailsItemModel itemModel;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 12,
-          height: 12,
-          decoration: ShapeDecoration(
-            shape: const OvalBorder(),
-            color: itemModel.color,
-          ),
+    return ListTile(
+      trailing: Container(
+        width: 12,
+        height: 12,
+        decoration: ShapeDecoration(
+          shape: const OvalBorder(),
+          color: itemModel.color,
         ),
-        const SizedBox(width: 12),
-        Text(
-          itemModel.text,
-          style: AppStyles.regular16.copyWith(color: kSecondaryColor),
-        ),
-        const Expanded(child: SizedBox()),
-        Text(
-          "${itemModel.value}%",
-          style: AppStyles.medium16.copyWith(color: const Color(0xff208CC8)),
-        ),
-      ],
+      ),
+      title: Text(
+        itemModel.text,
+        style: AppStyles.regular16.copyWith(color: kSecondaryColor),
+      ),
+      leading: Text(
+        "${itemModel.value}%",
+        style: AppStyles.medium16.copyWith(color: const Color(0xff208CC8)),
+      ),
     );
   }
 }
