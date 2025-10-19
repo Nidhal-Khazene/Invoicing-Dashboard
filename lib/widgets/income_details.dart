7641f0dc04dc8388591cc8839db26bfde07e660b
nidhal khazene <nidhal.khazene@gmail.com>
2025-10-19 12:12:50 +0100

    refactor income details

--- lib/widgets/income_details.dart
+++ lib/widgets/income_details.dart
@@ -1,33 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:invoicing_dashboard/widgets/income_details_item.dart';
+import 'package:invoicing_dashboard/widgets/income_details_item_model.dart';
 
 class IncomeDetails extends StatelessWidget {
   const IncomeDetails({super.key});
 
+  static final List<IncomeDetailsItemModel> items = [
+    IncomeDetailsItemModel(
+      color: const Color(0xFF208BC7),
+      text: "Design service",
+      value: 40,
+    ),
+    IncomeDetailsItemModel(
+      color: const Color(0xFF4DB7F2),
+      text: "Design product",
+      value: 25,
+    ),
+    IncomeDetailsItemModel(
+      color: const Color(0xFF064060),
+      text: "Product royalti",
+      value: 20,
+    ),
+    IncomeDetailsItemModel(
+      color: const Color(0xFFE2DECD),
+      text: "Other",
+      value: 22,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        IncomeDetailsItem(
-          color: Color(0xFF208BC7),
-          text: "Design service",
-          value: 40,
-        ),
-        SizedBox(height: 12),
-        IncomeDetailsItem(
-          color: Color(0xFF4DB7F2),
-          text: "Design service",
-          value: 25,
-        ),
-        SizedBox(height: 12),
-        IncomeDetailsItem(
-          color: Color(0xFF064060),
-          text: "Product royalti",
-          value: 20,
-        ),
-        SizedBox(height: 12),
-        IncomeDetailsItem(color: Color(0xFFE2DECD), text: "Other", value: 22),
-      ],
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return IncomeDetailsItem(itemModel: items[index]);
+      },
     );
   }
 }
