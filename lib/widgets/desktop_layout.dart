9688b1ebedb87fc7b7a05c5ab3a6f128e2929517
nidhal khazene <nidhal.khazene@gmail.com>
2025-10-21 10:04:27 +0100

    start fixing some errors: first we add custom scroll view to desktop layout...

--- lib/widgets/desktop_layout.dart
+++ lib/widgets/desktop_layout.dart
@@ -15,21 +15,28 @@ class DesktopLayout extends StatelessWidget {
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
         Expanded(
-          flex: 3,
-          child: Padding(
-            padding: EdgeInsets.only(top: 40),
-            child: AllExpensesAndQuickInvoiceSection(),
-          ),
-        ),
-        SizedBox(width: 24),
-        Expanded(
-          flex: 2,
-          child: Column(
+          flex: 5,
+          child: Row(
             children: [
-              SizedBox(height: 40),
-              CardAndTransectionViewSection(),
-              SizedBox(height: 24),
-              Expanded(child: IncomeSection()),
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: AllExpensesAndQuickInvoiceSection(),
+                ),
+              ),
+              SizedBox(width: 24),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    SizedBox(height: 40),
+                    CardAndTransectionViewSection(),
+                    SizedBox(height: 24),
+                    Expanded(child: IncomeSection()),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
