286d5fff2cb864183b869d809e5be2ee652b994c
nidhal khazene <nidhal.khazene@gmail.com>
2025-10-23 08:39:27 +0100

    add responsive font size functions

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:invoicing_dashboard/constants.dart';
+import 'package:invoicing_dashboard/utils/size_config.dart';
 
 abstract class AppStyles {
   static final TextStyle semiBold16 = TextStyle(
@@ -71,4 +72,24 @@ abstract class AppStyles {
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w500,
   );
+}
+
+double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < SizeConfig.tabletBreakPoint) {
+    return width / 550;
+  } else if (width < SizeConfig.desktopBreakPoint) {
+    return width / 1000;
+  } else {
+    return width / 1500;
+  }
 }
