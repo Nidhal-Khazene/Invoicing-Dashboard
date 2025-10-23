2ce9ee02127e6610a83360ed838b23e3bff66335
nidhal khazene <nidhal.khazene@gmail.com>
2025-10-23 08:54:02 +0100

    add the responsive font size function to all styles

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -7,70 +7,70 @@ import 'package:invoicing_dashboard/utils/size_config.dart';
 abstract class AppStyles {
   static final TextStyle semiBold16 = TextStyle(
     color: kSecondaryColor,
-    fontSize: 16,
+    fontSize: getResponsiveFontSize(fontSize: 16),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w600,
   );
 
   static final TextStyle regular12 = TextStyle(
     color: kGreyColor,
-    fontSize: 12,
+    fontSize: getResponsiveFontSize(fontSize: 12),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w400,
   );
 
   static final TextStyle bold16 = TextStyle(
     color: kSecondaryColor,
-    fontSize: 16,
+    fontSize: getResponsiveFontSize(fontSize: 16),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w700,
   );
 
   static final TextStyle regular16 = TextStyle(
     color: kGreyColor,
-    fontSize: 16,
+    fontSize: getResponsiveFontSize(fontSize: 16),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w400,
   );
 
   static final TextStyle semiBold20 = TextStyle(
     color: kSecondaryColor,
-    fontSize: 20,
+    fontSize: getResponsiveFontSize(fontSize: 20),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w600,
   );
 
   static final TextStyle medium16 = TextStyle(
     color: kSecondaryColor,
-    fontSize: 16,
+    fontSize: getResponsiveFontSize(fontSize: 16),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w500,
   );
 
   static final TextStyle regular14 = TextStyle(
     color: kGreyColor,
-    fontSize: 14,
+    fontSize: getResponsiveFontSize(fontSize: 14),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w400,
   );
 
   static final TextStyle semiBold24 = TextStyle(
     color: kPrimaryColor,
-    fontSize: 24,
+    fontSize: getResponsiveFontSize(fontSize: 24),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w600,
   );
 
   static final TextStyle semiBold18 = TextStyle(
     color: kSecondaryColor,
-    fontSize: 18,
+    fontSize: getResponsiveFontSize(fontSize: 18),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w600,
   );
 
   static final TextStyle medium20 = TextStyle(
     color: kSecondaryColor,
-    fontSize: 20,
+    fontSize: getResponsiveFontSize(fontSize: 20),
     fontFamily: kPrimaryFontFamily,
     fontWeight: FontWeight.w500,
   );
